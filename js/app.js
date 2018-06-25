fcd0f69161c5bd420d2fbbd5535dede0c257b2bf
rurigrass <ruripg@gmail.com>
2018-06-25 09:43:17 +0100

    more

--- js/app.js
+++ js/app.js
@@ -1,32 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
-const form = document.querySelector('#new-item-form')
+  const form = document.querySelector('#new-item-form')
+  const deleteAll = document.querySelector('#delete-all');
 
-const handleFormSave = function(event) {
-  event.preventDefault();
-  const form = event.target;
-  const name = form.name.value;
-  const species = form.species.value;
-  const continent = form.continent.value;
-  const result = document.querySelector('#endangered-animals');
+  const handleFormSave = function(event) {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const species = form.species.value;
+    const continent = form.continent.value;
+    const result = document.querySelector('#endangered-animals');
 
-  const paragraph = document.createElement('p');
-  paragraph.textContent = `Name: ${name}, Species: ${species}, Continent: ${continent}`
-  result.appendChild(paragraph);
+    const paragraph = document.createElement('p');
+    paragraph.textContent = `Name: ${name}, Species: ${species}, Continent: ${continent}`
+    result.appendChild(paragraph);
 
-  form.reset()
-}
+    form.reset()
+  }
 
-form.addEventListener('submit', handleFormSave)
+  const handleDeleteAll = function(event){
+    const animals = document.querySelector('#endangered-animals');
+    animals.innerHTML = ''
+  }
 
-const deleteAll = document.querySelector('#delete-all');
-
- const handleDeleteAll = function(event){
-   const animals = document.querySelector('#endangered-animals');
-   animals.innerHTML = ''
-}
-
- deleteAll.addEventListener('click',handleDeleteAll);
+  form.addEventListener('submit', handleFormSave);
+  deleteAll.addEventListener('click',handleDeleteAll);
 
 });
